8d5d8423d9e5d2e26f0723cb21206615dd76d6fb
yeeun <dpdmstjs@gmail.com>
2023-09-07 16:01:23 +0900

    init : 프로젝트 초기 환경 세팅

---
+++ src/main/java/com/javajober/javajoberserver/JavaJoberServerApplication.java
@@ -0,0 +1,13 @@
+package com.javajober.javajoberserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJoberServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJoberServerApplication.class, args);
+	}
+
+}

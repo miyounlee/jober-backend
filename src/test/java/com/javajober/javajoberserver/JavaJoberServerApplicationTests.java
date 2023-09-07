80261cb9a8601feeb521811cbe56ae1767ab54b9
yeeun <dpdmstjs@gmail.com>
2023-09-07 16:02:52 +0900

    init : 프로젝트 초기 환경 세팅

---
+++ src/test/java/com/javajober/javajoberserver/JavaJoberServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javajober.javajoberserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJoberServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

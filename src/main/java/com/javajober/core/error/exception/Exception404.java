1c747170a1fd2d251326e9cc8994b6f435c0aad2
Miyeon Lee <leeyun92@naver.com>
2023-09-21 15:07:41 +0900

    feat : Exception 클래스, ExceptionHandler 생성 (#1)

---
+++ src/main/java/com/javajober/core/error/exception/Exception404.java
@@ -0,0 +1,20 @@
+package com.javajober.core.error.exception;
+
+import com.javajober.core.util.ApiUtils;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class Exception404 extends RuntimeException {
+    public Exception404(String message) {
+        super(message);
+    }
+
+    public ApiUtils.ApiResult<?> body(){
+        return ApiUtils.error(getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    public HttpStatus status(){
+        return HttpStatus.NOT_FOUND;
+    }
+}
---
+++ src/main/java/com/javajober/core/error/exception/Exception500.java
@@ -0,0 +1,21 @@
+package com.javajober.core.error.exception;
+
+import com.javajober.core.util.ApiUtils;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class Exception500 extends RuntimeException {
+    public Exception500(String message) {
+        super(message);
+    }
+
+    public ApiUtils.ApiResult<?> body() {
+        return ApiUtils.error(getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    public HttpStatus status(){
+        return HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+
+}

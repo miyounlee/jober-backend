031b841805a68062babad925144e8334fc95d552
yunhyeonjin <h00j@naver.com>
2023-09-24 16:34:24 +0900

    feat: 템플릿 권한 설정 Controller(#11)

---
+++ src/main/java/com/javajober/controller/TemplateBlockController.java
@@ -0,0 +1,27 @@
+package com.javajober.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javajober.core.util.ApiUtils;
+import com.javajober.dto.MemberAuthResponse;
+import com.javajober.entity.SpaceType;
+import com.javajober.service.TemplateBlockService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/wall/templates")
+public class TemplateBlockController {
+	private final TemplateBlockService templateBlockService;
+
+	@GetMapping("/auth")
+	public ResponseEntity<?> getTemplateAuthList(@RequestParam SpaceType spaceType, @RequestParam Long memberId) {
+		MemberAuthResponse memberAuthResponse = templateBlockService.getTemplateAuthList(spaceType, memberId);
+		return ResponseEntity.ok(ApiUtils.success(memberAuthResponse));
+	}
+}

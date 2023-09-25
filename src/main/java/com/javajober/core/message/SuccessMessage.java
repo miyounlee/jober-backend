bc411e85a2af38f02b7ee8c50302a1e3729e272c
Miyeon Lee <leeyun92@naver.com>
2023-09-25 14:40:19 +0900

    feat : 성공메세지 추가 (#32)

--- src/main/java/com/javajober/core/message/SuccessMessage.java
+++ src/main/java/com/javajober/core/message/SuccessMessage.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 @Getter
 public enum SuccessMessage {
 
-	HOME_SUCCESS("홈 조회를 성공했습니다.");
+	HOME_SUCCESS("홈 조회를 성공했습니다."),
+	SUCCESS_TEMPORARY_SAVE_QUERY("임시 저장 조회를 성공했습니다."),
+	;
 
 	private final String message;
 

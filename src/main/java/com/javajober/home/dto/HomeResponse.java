2b6194713f1790f095e4ab516e8b863393a215fc
yeeun <dpdmstjs@gmail.com>
2023-09-25 11:45:40 +0900

    refactor : Map key -> SpaceType으로 변경 (#26)

--- src/main/java/com/javajober/home/dto/HomeResponse.java
+++ src/main/java/com/javajober/home/dto/HomeResponse.java
@@ -3,15 +3,17 @@ package com.javajober.home.dto;
 import java.util.List;
 import java.util.Map;
 
+import com.javajober.entity.SpaceType;
+
 import lombok.Getter;
 
 @Getter
 public class HomeResponse {
 
 	private MemberResponse member;
-	private Map<String, List<AddSpaceResponse>> spaceWall;
+	private Map<SpaceType, List<AddSpaceResponse>> spaceWall;
 
-	public HomeResponse(final MemberResponse member, final Map<String, List<AddSpaceResponse>> spaceWall) {
+	public HomeResponse(final MemberResponse member, final Map<SpaceType, List<AddSpaceResponse>> spaceWall) {
 		this.member = member;
 		this.spaceWall = spaceWall;
 	}

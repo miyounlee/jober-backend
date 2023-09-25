3e5d0b388483189ab76a2f262ab90003cb4a163c
yeeun <dpdmstjs@gmail.com>
2023-09-25 11:02:07 +0900

    feat : 홈 개인 정보 조회를 위한 MemberRepository 생성 (#26)

---
+++ src/main/java/com/javajober/member/repository/MemberRepository.java
@@ -0,0 +1,19 @@
+package com.javajober.member.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.Repository;
+
+import com.javajober.core.error.exception.Exception404;
+import com.javajober.core.message.ErrorMessage;
+import com.javajober.entity.Member;
+
+public interface MemberRepository extends Repository<Member, Long> {
+
+	Optional<Member> findById(Long id);
+
+	default Member findMember (final Long id) {
+		return findById(id)
+			.orElseThrow(() -> new Exception404(ErrorMessage.MEMBER_NOT_FOUND));
+	}
+}

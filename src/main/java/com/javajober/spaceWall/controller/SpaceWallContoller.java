910c36ca4098b30598246a7de8b04564c7659276
Miyeon Lee <leeyun92@naver.com>
2023-09-25 14:38:41 +0900

    feat : SpaceWallContoller 기능 구현 (#32)

---
+++ src/main/java/com/javajober/spaceWall/controller/SpaceWallContoller.java
@@ -0,0 +1,25 @@
+package com.javajober.spaceWall.controller;
+
+import com.javajober.core.util.ApiUtils;
+import com.javajober.spaceWall.dto.SpaceWallResponse;
+import com.javajober.spaceWall.service.SpaceWallService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class SpaceWallContoller {
+
+    private final SpaceWallService spaceWallService;
+
+    @GetMapping("/api/wall-temporary/storage/{memberId}/{addSpaceId}")
+    public ResponseEntity<?> checkSpaceWallTemporary(@PathVariable Long memberId, @PathVariable Long addSpaceId) {
+
+        SpaceWallResponse response = spaceWallService.checkSpaceWallTemporary(memberId, addSpaceId);
+
+        return ResponseEntity.ok(ApiUtils.success(response));
+    }
+}

5fac2f581b64827d0cd23f78e25541cb42694361
Miyeon Lee <leeyun92@naver.com>
2023-09-25 14:34:36 +0900

    feat : 에러메세지 추가 (#32)

--- src/main/java/com/javajober/core/message/ErrorMessage.java
+++ src/main/java/com/javajober/core/message/ErrorMessage.java
@@ -6,7 +6,11 @@ import lombok.Getter;
 public enum ErrorMessage {
 
 	MEMBER_NOT_FOUND("존재하지 않는 회원 정보입니다."),
-	ADD_SPACE_NOT_FOUND("존재하지 않는 스페이스 입니다.");
+	ADD_SPACE_NOT_FOUND("존재하지 않는 스페이스 입니다."),
+
+	FAILED_TO_DELETE_THE_FILE("파일 삭제를 실패하였습니다."),
+	SAVED_SPACE_WALL_ALREADY_EXISTS("저장된 공유페이지가 있습니다."),
+	;
 
 	private final String message;
 

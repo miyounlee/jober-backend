0a78630b14a143c31e95c95f1adb2aa30b949d02
yeeun <dpdmstjs@gmail.com>
2023-09-25 09:59:25 +0900

    feat : 에러 메세지 클래스 생성

---
+++ src/main/java/com/javajober/core/message/ErrorMessage.java
@@ -0,0 +1,16 @@
+package com.javajober.core.message;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorMessage {
+
+	MEMBER_NOT_FOUND("존재하지 않는 회원 정보입니다."),
+	ADD_SPACE_NOT_FOUND("존재하지 않는 스페이스 입니다.");
+
+	private final String message;
+
+	ErrorMessage(String message) {
+		this.message = message;
+	}
+}

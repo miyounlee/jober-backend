e2d57b38308787f1671242e4ddc39f2b29f574ce
yeeun <dpdmstjs@gmail.com>
2023-09-25 10:41:49 +0900

    refactor : Exception404 ErrorMessage 타입으로 수정

--- src/main/java/com/javajober/core/error/exception/Exception404.java
+++ src/main/java/com/javajober/core/error/exception/Exception404.java
@@ -1,20 +1,24 @@
 package com.javajober.core.error.exception;
 
+import com.javajober.core.message.ErrorMessage;
 import com.javajober.core.util.ApiUtils;
+
 import lombok.Getter;
+
 import org.springframework.http.HttpStatus;
 
 @Getter
 public class Exception404 extends RuntimeException {
-    public Exception404(String message) {
-        super(message);
+
+    public Exception404(ErrorMessage errorMessage) {
+        super(errorMessage.getMessage());
     }
 
-    public ApiUtils.ApiResult<?> body(){
+    public ApiUtils.ApiResult<?> body() {
         return ApiUtils.error(getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    public HttpStatus status(){
+    public HttpStatus status() {
         return HttpStatus.NOT_FOUND;
     }
 }

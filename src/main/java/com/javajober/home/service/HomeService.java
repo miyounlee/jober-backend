3079ab62c02241f14d4bb8ed429693220aba99a3
yeeun <dpdmstjs@gmail.com>
2023-09-25 11:15:35 +0900

    feat : 홈 조회 service 구현 (#26)

---
+++ src/main/java/com/javajober/home/service/HomeService.java
@@ -0,0 +1,51 @@
+package com.javajober.home.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.javajober.entity.Member;
+import com.javajober.entity.SpaceType;
+import com.javajober.home.dto.AddSpaceResponse;
+import com.javajober.home.dto.HomeResponse;
+import com.javajober.home.dto.MemberResponse;
+import com.javajober.addSpace.repository.AddSpaceRepository;
+import com.javajober.member.repository.MemberRepository;
+
+@Service
+public class HomeService {
+
+	private final AddSpaceRepository addSpaceRepository;
+	private final MemberRepository memberRepository;
+
+	public HomeService(AddSpaceRepository addSpaceRepository, MemberRepository memberRepository) {
+		this.addSpaceRepository = addSpaceRepository;
+		this.memberRepository = memberRepository;
+	}
+
+	public HomeResponse getHomes(Long memberId) {
+
+		Member member = memberRepository.findMember(memberId);
+
+		MemberResponse memberInfo = MemberResponse.from(member);
+
+		List<AddSpaceResponse> personalSpaces =
+			addSpaceRepository.findByMemberIdAndSpaceType(memberId, SpaceType.PERSONAL).stream()
+				.map(AddSpaceResponse::from)
+				.collect(Collectors.toList());
+
+		List<AddSpaceResponse> organizationSpaces =
+			addSpaceRepository.findByMemberIdAndSpaceType(memberId, SpaceType.ORGANIZATION).stream()
+				.map(AddSpaceResponse::from)
+				.collect(Collectors.toList());
+
+		Map<String, List<AddSpaceResponse>> spaces = new HashMap<>();
+		spaces.put("personal", personalSpaces);
+		spaces.put("organization", organizationSpaces);
+
+		return new HomeResponse(memberInfo, spaces);
+	}
+}

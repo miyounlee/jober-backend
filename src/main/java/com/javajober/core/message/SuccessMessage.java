3c14fd55b701edfd4244adf93460c14feb966516
Miyeon Lee <leeyun92@naver.com>
2023-09-26 13:26:53 +0900

    feat : 성공메세지 추가 (#43)

--- src/main/java/com/javajober/core/message/SuccessMessage.java
+++ src/main/java/com/javajober/core/message/SuccessMessage.java
@@ -6,12 +6,18 @@ import lombok.Getter;
 public enum SuccessMessage {
 
 	HOME_SUCCESS("홈 조회를 성공했습니다."),
-	SUCCESS_TEMPORARY_SAVE_QUERY("임시 저장 조회를 성공했습니다."),
+	SPACE_WALL_TEMPORARY_QUERY_SUCCESS("임시 저장 조회를 성공했습니다."),
 
 	TEMPLATE_AUTH_SUCCESS("권한설정을 위한 유저정보 조회를 성공했습니다."),
 	TEMPLATE_RECOMMEND_SUCCESS("추천템플릿 조회를 성공했습니다."),
 	TEMPLATE_SEARCH_SUCCESS("템플릿 검색을 성공했습니다."),
-	TEMPLATE_CATEGORY_SUCCESS("카테고리별 템플릿 리스트 조회를 성공했습니다.");
+	TEMPLATE_CATEGORY_SUCCESS("카테고리별 템플릿 리스트 조회를 성공했습니다."),
+
+	CREATE_SUCCESS("저장이 완료되었습니다."),
+	READ_SUCCESS("성공적으로 조회되었습니다."),
+	UPDATE_SUCCESS("성공적으로 수정되었습니다."),
+	DELETE_SUCCESS("성공적으로 삭제되었습니다.")
+	;
 
 	private final String message;
 

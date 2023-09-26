806ae2622691fa9ee047e173cd581898426f2743
yangsoohyun <cendy1234@naver.com>
2023-09-26 21:12:34 +0900

    feat: 구독 취소 repository 코드 구현 (#49)

--- src/main/java/com/javajober/fcm/repository/SubscriptionRepository.java
+++ src/main/java/com/javajober/fcm/repository/SubscriptionRepository.java
@@ -1,8 +1,16 @@
 package com.javajober.fcm.repository;
 
 import com.javajober.fcm.domain.Subscription;
+import com.javajober.member.domain.Member;
+import com.javajober.spaceWall.domain.SpaceWall;
 import org.springframework.data.repository.Repository;
+
+import java.util.Optional;
 
 public interface SubscriptionRepository extends Repository<Subscription, Long> {
     Subscription save(Subscription subscription);
+
+    Optional<Subscription> findBySubscriberAndSpaceWall(Member subscriber, SpaceWall spaceWall);
+
+    void delete(Subscription subscription);
 }

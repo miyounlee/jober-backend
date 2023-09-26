6e9ca3776ff89e5fc9882f4547cb84eb988a8115
Miyeon Lee <leeyun92@naver.com>
2023-09-26 13:24:01 +0900

    chore : RequiredArgsConstructor가 대신 커스텀 생성자로 수정 (#43)

--- src/main/java/com/javajober/spaceWall/controller/SpaceWallContoller.java
+++ src/main/java/com/javajober/spaceWall/controller/SpaceWallContoller.java
@@ -3,17 +3,19 @@ package com.javajober.spaceWall.controller;
 import com.javajober.core.util.ApiUtils;
 import com.javajober.spaceWall.dto.response.SpaceWallResponse;
 import com.javajober.spaceWall.service.SpaceWallService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
 @RestController
 public class SpaceWallContoller {
 
     private final SpaceWallService spaceWallService;
+
+    public SpaceWallContoller(SpaceWallService spaceWallService) {
+        this.spaceWallService = spaceWallService;
+    }
 
     @GetMapping("/api/wall-temporary/storage/{memberId}/{addSpaceId}")
     public ResponseEntity<?> checkSpaceWallTemporary(@PathVariable Long memberId, @PathVariable Long addSpaceId) {

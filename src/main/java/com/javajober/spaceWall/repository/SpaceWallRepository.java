ffe2ffc3acc721a3111897067f602d1c02a7681a
Miyeon Lee <leeyun92@naver.com>
2023-09-26 18:19:25 +0900

    modify : 공유페이지 임시 저장 내역 확인 로직 수정 (#43)

--- src/main/java/com/javajober/spaceWall/repository/SpaceWallRepository.java
+++ src/main/java/com/javajober/spaceWall/repository/SpaceWallRepository.java
@@ -1,23 +1,16 @@
 package com.javajober.spaceWall.repository;
 
-import com.javajober.core.error.exception.Exception404;
-import com.javajober.core.message.ErrorMessage;
 import com.javajober.spaceWall.domain.SpaceWall;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface SpaceWallRepository extends Repository<SpaceWall, Long> {
 
     @Query("SELECT s FROM SpaceWall s LEFT JOIN s.addSpace a " +
             "WHERE a.id = :addSpaceId AND a.member.id = :memberId")
-    Optional<SpaceWall> findSpaceWall(@Param("memberId") Long memberId, @Param("addSpaceId") Long addSpaceId);
+    List<SpaceWall> findSpaceWall(@Param("memberId") Long memberId, @Param("addSpaceId") Long addSpaceId);
 
-    default SpaceWall getById(final Long memberId, final Long addSpaceId) {
-
-        return findSpaceWall(memberId, addSpaceId)
-                .orElseThrow(() -> new Exception404(ErrorMessage.ADD_SPACE_NOT_FOUND));
-    }
 }
--- src/main/java/com/javajober/spaceWall/service/SpaceWallService.java
+++ src/main/java/com/javajober/spaceWall/service/SpaceWallService.java
@@ -1,12 +1,14 @@
 package com.javajober.spaceWall.service;
 
-import com.javajober.core.error.exception.Exception500;
+import com.javajober.core.error.exception.Exception400;
 import com.javajober.core.message.ErrorMessage;
 import com.javajober.spaceWall.domain.FlagType;
 import com.javajober.spaceWall.domain.SpaceWall;
 import com.javajober.spaceWall.dto.response.SpaceWallResponse;
 import com.javajober.spaceWall.repository.SpaceWallRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SpaceWallService {
@@ -19,16 +21,21 @@ public class SpaceWallService {
 
     public SpaceWallResponse checkSpaceWallTemporary(Long memberId, Long addSpaceId) {
 
-        SpaceWall spaceWallPS = spaceWallRepository.getById(memberId, addSpaceId);
+        List<SpaceWall> spaceWalls = spaceWallRepository.findSpaceWall(memberId, addSpaceId);
 
-        if (spaceWallPS.getFlag().equals(FlagType.PENDING)) {
-            return SpaceWallResponse.from(spaceWallPS.getId(), true);
+        if (spaceWalls == null || spaceWalls.isEmpty()) {
+            return SpaceWallResponse.from(null, false);
         }
 
-        if (spaceWallPS.getFlag().equals(FlagType.SAVED)) {
-            throw new Exception500(ErrorMessage.SAVED_SPACE_WALL_ALREADY_EXISTS);
-        }
+        for (SpaceWall spaceWall : spaceWalls) {
 
+            if (spaceWall.getFlag().equals(FlagType.PENDING) && spaceWall.getDeletedAt() == null) {
+                return SpaceWallResponse.from(spaceWall.getId(), true);
+            }
+            if (spaceWall.getFlag().equals(FlagType.SAVED) && spaceWall.getDeletedAt() == null) {
+                throw new Exception400(ErrorMessage.SAVED_SPACE_WALL_ALREADY_EXISTS);
+            }
+        }
         return SpaceWallResponse.from(null, false);
     }
 }

d423f3aa3a17a1c7c7046d3cbe6240b5277b3825
Miyeon Lee <leeyun92@naver.com>
2023-09-27 14:54:56 +0900

    feat : 자유 블록 저장을 위한 FreeBlockSaveRequestDTO 생성 (#38)

---
+++ src/main/java/com/javajober/freeBlock/dto/request/FreeBlockSaveRequest.java
@@ -0,0 +1,21 @@
+package com.javajober.freeBlock.dto.request;
+
+import com.javajober.freeBlock.domain.FreeBlock;
+import lombok.Getter;
+
+@Getter
+public class FreeBlockSaveRequest {
+
+    private String freeTitle;
+    private String freeContent;
+
+    public FreeBlockSaveRequest() {
+    }
+
+    public static FreeBlock toEntity(FreeBlockSaveRequest freeBlockSaveRequest) {
+        return FreeBlock.builder()
+                .freeTitle(freeBlockSaveRequest.getFreeTitle())
+                .freeContent(freeBlockSaveRequest.getFreeContent())
+                .build();
+    }
+}

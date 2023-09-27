717af0a9084c59c77962bcc81c6d8d4b17fe1946
Miyeon Lee <leeyun92@naver.com>
2023-09-27 14:57:39 +0900

    feat : 자유 블록 수정을 위한 FreeBlockUpdateRequestsDTO 생성 (#38)

---
+++ src/main/java/com/javajober/freeBlock/dto/request/FreeBlockUpdateRequest.java
@@ -0,0 +1,23 @@
+package com.javajober.freeBlock.dto.request;
+
+import com.javajober.freeBlock.domain.FreeBlock;
+import lombok.Getter;
+
+
+@Getter
+public class FreeBlockUpdateRequest {
+
+    private Long freeId;
+    private String freeTitle;
+    private String freeContent;
+
+    public FreeBlockUpdateRequest() {
+    }
+
+    public static FreeBlock toEntity(FreeBlockUpdateRequest freeBlockUpdateRequest) {
+        return FreeBlock.builder()
+                .freeTitle(freeBlockUpdateRequest.getFreeTitle())
+                .freeContent(freeBlockUpdateRequest.getFreeContent())
+                .build();
+    }
+}

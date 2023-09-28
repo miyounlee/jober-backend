81b743805e24be961e058590bfcba077dab26729
yeeun <dpdmstjs@gmail.com>
2023-09-28 18:20:50 +0900

    feat : SpaceWallController save 메서드 구현 (#64)

--- src/main/java/com/javajober/spaceWall/controller/SpaceWallController.java
+++ src/main/java/com/javajober/spaceWall/controller/SpaceWallController.java
@@ -2,14 +2,19 @@ package com.javajober.spaceWall.controller;
 
 import com.javajober.core.message.SuccessMessage;
 import com.javajober.core.util.ApiUtils;
+import com.javajober.spaceWall.dto.request.SpaceWallRequest;
 import com.javajober.spaceWall.dto.response.SpaceWallResponse;
 import com.javajober.spaceWall.service.SpaceWallService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api")
 @RestController
 public class SpaceWallController {
 
@@ -19,11 +24,19 @@ public class SpaceWallController {
         this.spaceWallService = spaceWallService;
     }
 
-    @GetMapping("/api/wall-temporary/storage/{memberId}/{addSpaceId}")
+    @GetMapping("/wall-temporary/storage/{memberId}/{addSpaceId}")
     public ResponseEntity<ApiUtils.ApiResponse<SpaceWallResponse>> checkSpaceWallTemporary(@PathVariable Long memberId, @PathVariable Long addSpaceId) {
 
         SpaceWallResponse response = spaceWallService.checkSpaceWallTemporary(memberId, addSpaceId);
 
         return ResponseEntity.ok(ApiUtils.success(HttpStatus.OK, SuccessMessage.SPACE_WALL_TEMPORARY_QUERY_SUCCESS, response));
+    }
+
+    @PostMapping("/wall")
+    public ResponseEntity<?> save(@RequestBody final SpaceWallRequest spaceWallRequest) {
+
+        spaceWallService.save(spaceWallRequest);
+
+        return ResponseEntity.ok(ApiUtils.success(HttpStatus.OK, SuccessMessage.CREATE_SUCCESS, null));
     }
 }

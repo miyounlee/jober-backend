e91f287b020cc99b2b0715bd67b72dcf4e1085e2
Miyeon Lee <leeyun92@naver.com>
2023-09-28 23:02:33 +0900

    feat : 파일 저장 경로 config (#68)
    - OS 별 저장 경로 예시
    (window) : C:\Users\[username]\JavaJober\20230928\
    (max) : /Users/[username]/JavaJober/20230928/
    (Linux) : /home/[username]/JavaJober/20230928/

---
+++ src/main/java/com/javajober/core/config/FileDirectoryConfig.java
@@ -0,0 +1,31 @@
+package com.javajober.core.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class FileDirectoryConfig {
+
+    public String getDirectoryPath() {
+
+        String separator = File.separator;
+        LocalDate today = LocalDate.now();
+
+        String directoryName = today.format(DateTimeFormatter.ofPattern("yyyyMMdd"));   // 20230928
+        String directoryPath = System.getProperty("user.home") + separator + "JavaJober" + separator +
+                directoryName + separator;
+
+        createDirectory(directoryPath);
+        return directoryPath;
+    }
+
+    public void createDirectory(String directoryPath) {
+        File directory = new File(directoryPath);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+    }
+}

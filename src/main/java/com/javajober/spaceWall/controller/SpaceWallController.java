a8c0f7dac68aedc61b25df03a0591fb69036efaa
Miyeon Lee <leeyun92@naver.com>
2023-09-30 00:42:41 +0900

    chore : multipartFile 요청 시 타입 지정을 위한 설정 추가 (#76)

--- src/main/java/com/javajober/spaceWall/controller/SpaceWallController.java
+++ src/main/java/com/javajober/spaceWall/controller/SpaceWallController.java
@@ -8,6 +8,7 @@ import com.javajober.spaceWall.dto.request.SpaceWallRequest;
 import com.javajober.spaceWall.dto.response.SpaceWallResponse;
 import com.javajober.spaceWall.service.SpaceWallService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -33,7 +34,7 @@ public class SpaceWallController {
         return ResponseEntity.ok(ApiUtils.success(HttpStatus.OK, SuccessMessage.SPACE_WALL_TEMPORARY_QUERY_SUCCESS, response));
     }
 
-    @PostMapping("/wall")
+    @PostMapping(path = "/wall", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<?> save(@RequestPart(value = "data") final SpaceWallRequest spaceWallRequest,
                                   @RequestPart(value = "fileName", required = false) List<MultipartFile> files,
                                   @RequestPart(value = "backgroundImgURL", required = false) MultipartFile backgroundImgURL,

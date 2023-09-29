bb74df778bc5551e71c710aa70086464f48e5376
Miyeon Lee <leeyun92@naver.com>
2023-09-30 00:43:02 +0900

    feat : CORS 설정 추가 (#76)

--- src/main/java/com/javajober/core/config/WebMvcConfig.java
+++ src/main/java/com/javajober/core/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.javajober.core.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,5 +21,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry
                 .addResourceHandler("/files/**")
                 .addResourceLocations("file:" + fileDirectoryConfig.getDirectoryPath());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://java-jober.netlify.app", "http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }

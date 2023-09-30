105d03aaf515ab469648c9608245924948b0198e
yunhyeonjin <h00j@naver.com>
2023-09-30 21:06:01 +0900

    chore: styleSetting 추가(#70)

--- src/main/java/com/javajober/spaceWall/dto/request/DataRequest.java
+++ src/main/java/com/javajober/spaceWall/dto/request/DataRequest.java
@@ -2,6 +2,7 @@ package com.javajober.spaceWall.dto.request;
 
 import java.util.List;
 
+import com.javajober.setting.dto.StyleSettingSaveRequest;
 import com.javajober.wallInfoBlock.dto.request.WallInfoBlockRequest;
 import lombok.Getter;
 
@@ -12,15 +13,17 @@ public class DataRequest {
 	private String shareURL;
 	private WallInfoBlockRequest wallInfoBlock;
 	private List<BlockRequest> blocks;
+	private StyleSettingSaveRequest styleSetting;
   
 	private DataRequest() {
 	}
 
-	public DataRequest(final String category, final Long memberId, final String shareURL, final WallInfoBlockRequest wallInfoBlock, final List<BlockRequest> blocks) {
+	public DataRequest(final String category, final Long memberId, final String shareURL, final WallInfoBlockRequest wallInfoBlock, final List<BlockRequest> blocks, final StyleSettingSaveRequest styleSetting) {
 		this.category = category;
 		this.memberId = memberId;
 		this.shareURL = shareURL;
 		this.wallInfoBlock = wallInfoBlock;
 		this.blocks = blocks;
+		this.styleSetting = styleSetting;
 	}
 }

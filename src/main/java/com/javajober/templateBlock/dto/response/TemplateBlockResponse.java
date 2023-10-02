c6a48d4f7a7c8584f68e385f9c20ae36c83b4f0f
Miyeon Lee <leeyun92@naver.com>
2023-10-03 01:38:35 +0900

    modify : 블록 Response에서 CommonResponse를 구현하도록 수정 및 각 블록 pk 응답하도록 수정

--- src/main/java/com/javajober/templateBlock/dto/response/TemplateBlockResponse.java
+++ src/main/java/com/javajober/templateBlock/dto/response/TemplateBlockResponse.java
@@ -3,41 +3,48 @@ package com.javajober.templateBlock.dto.response;
 
 import java.util.List;
 
+import com.javajober.core.util.CommonResponse;
 import com.javajober.templateBlock.domain.TemplateBlock;
 
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-public class TemplateBlockResponse {
-	private String templateUUID;
-	private String templateTitle;
-	private String templateDescription;
-	private List<Long> hasAccessTemplateAuth;
-	private List<Long> hasDenyTemplateAuth;
+public class TemplateBlockResponse implements CommonResponse {
 
-	private TemplateBlockResponse(){
+    private Long templateBlockId;
+    private String templateUUID;
+    private String templateTitle;
+    private String templateDescription;
+    private List<Long> hasAccessTemplateAuth;
+    private List<Long> hasDenyTemplateAuth;
 
-	}
+    private TemplateBlockResponse() {
 
-	@Builder
-	public TemplateBlockResponse(String templateUUID, String templateTitle, String templateDescription,List<Long> hasAccessTemplateAuth,
-		List<Long> hasDenyTemplateAuth){
-		this.templateUUID=templateUUID;
-		this.templateTitle=templateTitle;
-		this.templateDescription=templateDescription;
-		this.hasAccessTemplateAuth = hasAccessTemplateAuth;
-		this.hasDenyTemplateAuth = hasDenyTemplateAuth;
-	}
+    }
 
-	public static TemplateBlockResponse from(TemplateBlock templateBlock,List<Long> hasAccessTemplateAuth,
-		List<Long> hasDenyTemplateAuth){
-		return TemplateBlockResponse.builder()
-			.templateUUID(templateBlock.getTemplateUUID())
-			.templateTitle(templateBlock.getTemplateTitle())
-			.templateDescription(templateBlock.getTemplateDescription())
-			.hasAccessTemplateAuth(hasAccessTemplateAuth)
-			.hasDenyTemplateAuth(hasDenyTemplateAuth)
-			.build();
-	}
+    @Builder
+    public TemplateBlockResponse(final Long templateBlockId, final String templateUUID, final String templateTitle,
+                                 final String templateDescription, final List<Long> hasAccessTemplateAuth,
+                                 final List<Long> hasDenyTemplateAuth) {
+        this.templateBlockId = templateBlockId;
+        this.templateUUID = templateUUID;
+        this.templateTitle = templateTitle;
+        this.templateDescription = templateDescription;
+        this.hasAccessTemplateAuth = hasAccessTemplateAuth;
+        this.hasDenyTemplateAuth = hasDenyTemplateAuth;
+    }
+
+    public static TemplateBlockResponse from(TemplateBlock templateBlock, List<Long> hasAccessTemplateAuth,
+                                             List<Long> hasDenyTemplateAuth) {
+
+        return TemplateBlockResponse.builder()
+                .templateBlockId(templateBlock.getId())
+                .templateUUID(templateBlock.getTemplateUUID())
+                .templateTitle(templateBlock.getTemplateTitle())
+                .templateDescription(templateBlock.getTemplateDescription())
+                .hasAccessTemplateAuth(hasAccessTemplateAuth)
+                .hasDenyTemplateAuth(hasDenyTemplateAuth)
+                .build();
+    }
 }

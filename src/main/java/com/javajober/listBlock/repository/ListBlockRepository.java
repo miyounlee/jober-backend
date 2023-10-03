9320eec04c76d9d10a2ea5a0c5e12a1599278f43
Miyeon Lee <leeyun92@naver.com>
2023-10-03 13:33:24 +0900

    feat : 각 블록의 Respository에 조회 메서드 추가 (#105)

--- src/main/java/com/javajober/listBlock/repository/ListBlockRepository.java
+++ src/main/java/com/javajober/listBlock/repository/ListBlockRepository.java
@@ -1,11 +1,22 @@
 package com.javajober.listBlock.repository;
 
+import com.javajober.core.error.exception.Exception404;
+import com.javajober.core.message.ErrorMessage;
 import org.springframework.data.repository.Repository;
 
 import com.javajober.listBlock.domain.ListBlock;
 
+import java.util.Optional;
+
 public interface ListBlockRepository extends Repository<ListBlock, Long> {
 
 	ListBlock save(ListBlock listBlock);
+
+	Optional<ListBlock> findByIdAndDeletedAtIsNull(Long id);
+
+	default ListBlock getById(final Long id) {
+		return findByIdAndDeletedAtIsNull(id)
+				.orElseThrow(() -> new Exception404(ErrorMessage.LIST_BLOCK_NOT_FOUND));
+	}
 
 }
--- src/main/java/com/javajober/spaceWall/repository/SpaceWallRepository.java
+++ src/main/java/com/javajober/spaceWall/repository/SpaceWallRepository.java
@@ -8,6 +8,7 @@ import com.javajober.core.error.exception.Exception404;
 import com.javajober.core.message.ErrorMessage;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SpaceWallRepository extends Repository<SpaceWall, Long> {
 
@@ -16,7 +17,7 @@ public interface SpaceWallRepository extends Repository<SpaceWall, Long> {
     List<SpaceWall> findSpaceWalls(@Param("memberId") Long memberId, @Param("addSpaceId") Long addSpaceId);
 
     boolean existsByAddSpaceId(Long addSpaceId);
-  
+
     SpaceWall save(SpaceWall spaceWall);
 
     default SpaceWall getById(final Long memberId, final Long spaceWallId) {
@@ -24,4 +25,13 @@ public interface SpaceWallRepository extends Repository<SpaceWall, Long> {
                 .findFirst()
                 .orElseThrow(() -> new Exception404(ErrorMessage.ADD_SPACE_NOT_FOUND));
     }
+
+    Optional<SpaceWall> findByIdAndAddSpace_IdAndMember_Id(Long id, Long addSpaceId, Long memberId);
+
+    default SpaceWall findSpaceWall(Long id, Long addSpaceId, Long memberId) {
+        return findByIdAndAddSpace_IdAndMember_Id(id, addSpaceId, memberId)
+                .orElseThrow(() -> new Exception404(ErrorMessage.SPACE_WALL_NOT_FOUND));
+    }
+
+
 }

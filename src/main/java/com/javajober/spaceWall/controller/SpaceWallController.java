29b05779572c18c47154123e53e28e15f5a21123
Miyeon Lee <leeyun92@naver.com>
2023-10-03 13:34:11 +0900

    feat : SpaceWallController에 조회 메서드 추가 (#105)

--- src/main/java/com/javajober/spaceWall/controller/SpaceWallController.java
+++ src/main/java/com/javajober/spaceWall/controller/SpaceWallController.java
@@ -1,10 +1,13 @@
 package com.javajober.spaceWall.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.javajober.core.message.SuccessMessage;
 import com.javajober.core.util.ApiUtils;
 import com.javajober.spaceWall.domain.FlagType;
 import com.javajober.spaceWall.dto.request.SpaceWallRequest;
 import com.javajober.spaceWall.dto.response.SpaceWallResponse;
+import com.javajober.spaceWall.dto.response.SpaceWallTemporaryResponse;
+import com.javajober.spaceWall.service.SpaceWallFindService;
 import com.javajober.spaceWall.service.SpaceWallService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,17 +19,19 @@ import org.springframework.web.bind.annotation.*;
 public class SpaceWallController {
 
     private final SpaceWallService spaceWallService;
+    private final SpaceWallFindService spaceWallFindService;
 
-    public SpaceWallController(SpaceWallService spaceWallService) {
+    public SpaceWallController(final SpaceWallService spaceWallService, final SpaceWallFindService spaceWallFindService) {
         this.spaceWallService = spaceWallService;
+        this.spaceWallFindService = spaceWallFindService;
     }
 
     @GetMapping("/wall-temporary/storage/{memberId}/{addSpaceId}")
-    public ResponseEntity<ApiUtils.ApiResponse<SpaceWallResponse>> checkSpaceWallTemporary(@PathVariable Long memberId, @PathVariable Long addSpaceId) {
+    public ResponseEntity<ApiUtils.ApiResponse<SpaceWallTemporaryResponse>> checkSpaceWallTemporary(@PathVariable Long memberId, @PathVariable Long addSpaceId) {
 
-        SpaceWallResponse response = spaceWallService.checkSpaceWallTemporary(memberId, addSpaceId);
+        SpaceWallTemporaryResponse response = spaceWallService.checkSpaceWallTemporary(memberId, addSpaceId);
 
-        return ResponseEntity.ok(ApiUtils.success(HttpStatus.OK, SuccessMessage.SPACE_WALL_TEMPORARY_QUERY_SUCCESS, response));
+        return ResponseEntity.ok(ApiUtils.success(HttpStatus.OK, SuccessMessage.SPACE_WALL_TEMPORARY_READ_SUCCESS, response));
     }
 
     @PostMapping("/wall")
@@ -44,6 +49,14 @@ public class SpaceWallController {
         spaceWallService.save(spaceWallRequest, FlagType.PENDING);
 
         return ResponseEntity.ok(ApiUtils.success(HttpStatus.OK, SuccessMessage.CREATE_SUCCESS, null));
+    }
+
+    @GetMapping("/wall/{memberId}/{addSpaceId}/{spaceWallId}")
+    public ResponseEntity<ApiUtils.ApiResponse<SpaceWallResponse>> find (
+            @PathVariable Long memberId, @PathVariable Long addSpaceId, @PathVariable Long spaceWallId) throws JsonProcessingException {
+
+        SpaceWallResponse data = spaceWallFindService.find(memberId, addSpaceId, spaceWallId);
+        return ResponseEntity.ok(ApiUtils.success(HttpStatus.OK, SuccessMessage.SPACE_WALL_READ_SUCCESS, data));
     }
 
 }

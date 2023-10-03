26a010d4e06e5515164380bcb6629bc817ec4ec6
Miyeon Lee <leeyun92@naver.com>
2023-10-03 13:32:21 +0900

    feat : BlockResponse 생성 (#105)
    - CommonResponse 구현체만 올수있도록 타입 지정

---
+++ src/main/java/com/javajober/spaceWall/dto/response/BlockResponse.java
@@ -0,0 +1,25 @@
+package com.javajober.spaceWall.dto.response;
+
+import com.javajober.core.util.CommonResponse;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class BlockResponse<T extends CommonResponse> {
+
+    private String blockUUID;
+    private String blockType;
+    private List<T> subData;
+
+    private BlockResponse() {}
+
+    @Builder
+    public BlockResponse(final String blockUUID, final String blockType, final List<T> subData) {
+        this.blockUUID = blockUUID;
+        this.blockType = blockType;
+        this.subData = subData;
+    }
+
+}

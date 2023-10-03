6bac975482bff11bf594765cc62b567420e045a6
Miyeon Lee <leeyun92@naver.com>
2023-10-03 13:33:24 +0900

    feat : 각 블록의 Respository에 조회 메서드 추가 (#105)

--- src/main/java/com/javajober/styleSetting/repository/StyleSettingRepository.java
+++ src/main/java/com/javajober/styleSetting/repository/StyleSettingRepository.java
@@ -1,10 +1,21 @@
 package com.javajober.styleSetting.repository;
 
+import com.javajober.core.error.exception.Exception404;
+import com.javajober.core.message.ErrorMessage;
 import org.springframework.data.repository.Repository;
 
 import com.javajober.styleSetting.domain.StyleSetting;
 
+import java.util.Optional;
+
 public interface StyleSettingRepository extends Repository<StyleSetting, Long> {
 
 	StyleSetting save(StyleSetting styleSetting);
+
+	Optional<StyleSetting> findByIdAndDeletedAtIsNull(Long id);
+
+	default StyleSetting getById(final Long id) {
+		return findByIdAndDeletedAtIsNull(id)
+				.orElseThrow(() -> new Exception404(ErrorMessage.STYLE_SETTING_BLOCK_NOT_FOUND));
+	}
 }

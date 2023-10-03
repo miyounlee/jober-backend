08c057477f1b164ec621fd5336e4830dac856b63
Miyeon Lee <leeyun92@naver.com>
2023-10-03 13:39:50 +0900

    feat : WallInfoBlockRepository에 조회 메서드 추가 (#105)

--- src/main/java/com/javajober/wallInfoBlock/repository/WallInfoBlockRepository.java
+++ src/main/java/com/javajober/wallInfoBlock/repository/WallInfoBlockRepository.java
@@ -1,9 +1,21 @@
 package com.javajober.wallInfoBlock.repository;
 
+import com.javajober.core.error.exception.Exception404;
+import com.javajober.core.message.ErrorMessage;
 import org.springframework.data.repository.Repository;
 
 import com.javajober.wallInfoBlock.domain.WallInfoBlock;
 
+import java.util.Optional;
+
 public interface WallInfoBlockRepository extends Repository<WallInfoBlock, Long> {
+
 	WallInfoBlock save(WallInfoBlock wallInfoBlock);
+
+	Optional<WallInfoBlock> findByIdAndDeletedAtIsNull(Long id);
+
+	default WallInfoBlock getById(final Long id) {
+		return findByIdAndDeletedAtIsNull(id)
+				.orElseThrow(() -> new Exception404(ErrorMessage.FILE_BLOCK_NOT_FOUND));
+	}
 }

5e1ea54ab5dfe63f54b0378f66673eaea6cd547c
yunhyeonjin <h00j@naver.com>
2023-10-03 21:05:46 +0900

    chore: DataUpdateRequest 수정(#111)

--- src/main/java/com/javajober/spaceWall/dto/request/DataUpdateRequest.java
+++ src/main/java/com/javajober/spaceWall/dto/request/DataUpdateRequest.java
@@ -3,7 +3,9 @@ package com.javajober.spaceWall.dto.request;
 import java.util.List;
 
 import com.javajober.styleSetting.dto.request.StyleSettingSaveRequest;
+import com.javajober.styleSetting.dto.request.StyleSettingUpdateRequest;
 import com.javajober.wallInfoBlock.dto.request.WallInfoBlockRequest;
+import com.javajober.wallInfoBlock.dto.request.WallInfoBlockUpdateRequest;
 
 import lombok.Getter;
 
@@ -11,14 +13,14 @@ import lombok.Getter;
 public class DataUpdateRequest {
 	private Long spaceWallId;
 	private String shareURL;
-	private WallInfoBlockRequest wallInfoBlock;
+	private WallInfoBlockUpdateRequest wallInfoBlock;
 	private List<BlockRequest> blocks;
-	private StyleSettingSaveRequest styleSetting;
+	private StyleSettingUpdateRequest styleSetting;
 
 	private DataUpdateRequest(){
 	}
 
-	public DataUpdateRequest(final Long spaceWallId, final String shareURL, final WallInfoBlockRequest wallInfoBlock, final List<BlockRequest> blocks, final StyleSettingSaveRequest styleSetting) {
+	public DataUpdateRequest(final Long spaceWallId, final String shareURL, final WallInfoBlockUpdateRequest wallInfoBlock, final List<BlockRequest> blocks, final StyleSettingUpdateRequest styleSetting) {
 		this.spaceWallId = spaceWallId;
 		this.shareURL = shareURL;
 		this.wallInfoBlock = wallInfoBlock;

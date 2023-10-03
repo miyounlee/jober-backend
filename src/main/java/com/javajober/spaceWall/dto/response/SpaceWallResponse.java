d4c26356b9637ee80f5cc04fdad446dbdf622a93
Miyeon Lee <leeyun92@naver.com>
2023-10-03 13:36:09 +0900

    modify : SpaceWallResponse를 임시 저장 응답 dto에서 공유페이지 조회 응답 dto로 수정 (#105)

--- src/main/java/com/javajober/spaceWall/dto/response/SpaceWallResponse.java
+++ src/main/java/com/javajober/spaceWall/dto/response/SpaceWallResponse.java
@@ -1,22 +1,50 @@
 package com.javajober.spaceWall.dto.response;
 
+import com.javajober.core.util.CommonResponse;
+import com.javajober.styleSetting.dto.response.StyleSettingResponse;
+import com.javajober.wallInfoBlock.dto.response.WallInfoBlockResponse;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class SpaceWallResponse {
 
-    private Long spaceWallId;
-    private boolean hasWallTemporary;
+    private String category;
+    private Long memberId;
+    private Long addSpaceId;
+    private String shareURL;
+    private WallInfoBlockResponse wallInfoBlockResponse;
+    private List<BlockResponse<CommonResponse>> blocks;
+    private StyleSettingResponse styleSetting;
 
-    private SpaceWallResponse() {
-
-    }
+    private SpaceWallResponse() {}
 
     @Builder
-    public SpaceWallResponse(final Long spaceWallId, final boolean hasWallTemporary) {
-        this.spaceWallId = spaceWallId;
-        this.hasWallTemporary = hasWallTemporary;
+    public SpaceWallResponse(final String category, final Long memberId, final Long addSpaceId,
+                             final String shareURL, final WallInfoBlockResponse wallInfoBlockResponse, final List<BlockResponse<CommonResponse>> blocks,
+                             final StyleSettingResponse styleSetting) {
+        this.category = category;
+        this.memberId = memberId;
+        this.addSpaceId = addSpaceId;
+        this.shareURL = shareURL;
+        this.wallInfoBlockResponse = wallInfoBlockResponse;
+        this.blocks = blocks;
+        this.styleSetting = styleSetting;
+    }
+
+    public static SpaceWallResponse from(final String category, final Long memberId, final Long addSpaceId,
+                                         final String shareURL, final WallInfoBlockResponse wallInfoBlockResponse, final List<BlockResponse<CommonResponse>> blocks,
+                                         final StyleSettingResponse styleSetting) {
+        return SpaceWallResponse.builder()
+                .category(category)
+                .memberId(memberId)
+                .addSpaceId(addSpaceId)
+                .shareURL(shareURL)
+                .wallInfoBlockResponse(wallInfoBlockResponse)
+                .blocks(blocks)
+                .styleSetting(styleSetting)
+                .build();
     }
 }
-

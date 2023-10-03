dcd25d11dae3c68871563e73d37ce67eff6d4a1c
Miyeon Lee <leeyun92@naver.com>
2023-10-03 17:05:49 +0900

    modify : 응답에 파일명 String으로 추가 (#110)

--- src/main/java/com/javajober/wallInfoBlock/dto/response/WallInfoBlockResponse.java
+++ src/main/java/com/javajober/wallInfoBlock/dto/response/WallInfoBlockResponse.java
@@ -10,15 +10,20 @@ public class WallInfoBlockResponse {
     private Long wallInfoBlockId;
     private String wallInfoTitle;
     private String wallInfoDescription;
+    private String wallInfoImgURL;
+    private String backgroundImgURL;
 
     public WallInfoBlockResponse() {
     }
 
     @Builder
-    public WallInfoBlockResponse(final Long wallInfoBlockId, final String wallInfoTitle, final String wallInfoDescription) {
+    public WallInfoBlockResponse(final Long wallInfoBlockId, final String wallInfoTitle, final String wallInfoDescription,
+                                 final String wallInfoImgURL, final String backgroundImgURL) {
         this.wallInfoBlockId = wallInfoBlockId;
         this.wallInfoTitle = wallInfoTitle;
         this.wallInfoDescription = wallInfoDescription;
+        this.wallInfoImgURL = wallInfoImgURL;
+        this.backgroundImgURL = backgroundImgURL;
     }
 
     public static WallInfoBlockResponse from(final WallInfoBlock wallInfoBlock) {
@@ -26,6 +31,8 @@ public class WallInfoBlockResponse {
                 .wallInfoBlockId(wallInfoBlock.getId())
                 .wallInfoTitle(wallInfoBlock.getWallInfoTitle())
                 .wallInfoDescription(wallInfoBlock.getWallInfoDescription())
+                .wallInfoImgURL(wallInfoBlock.getWallInfoProfileImageUrl())
+                .backgroundImgURL(wallInfoBlock.getWallInfoBackgroundImageUrl())
                 .build();
     }
 }

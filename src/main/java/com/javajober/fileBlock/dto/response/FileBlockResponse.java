ebb65eb05c1461d18825844549745e5e0f73855d
YeEun Sun <dpdmstjs@gmail.com>
2023-10-04 23:08:09 +0900

    FileBlockResponse file 필드 추가 완료

--- src/main/java/com/javajober/fileBlock/dto/response/FileBlockResponse.java
+++ src/main/java/com/javajober/fileBlock/dto/response/FileBlockResponse.java
@@ -12,17 +12,19 @@ public class FileBlockResponse implements CommonResponse {
     private String fileTitle;
     private String fileDescription;
     private String fileName;
+    private String file;
 
     private FileBlockResponse() {
 
     }
 
     @Builder
-    public FileBlockResponse(final Long fileBlockId, final String fileTitle, final String fileDescription, final String fileName) {
+    public FileBlockResponse(final Long fileBlockId, final String fileTitle, final String fileDescription, final String fileName, final String file) {
         this.fileBlockId = fileBlockId;
         this.fileTitle = fileTitle;
         this.fileDescription = fileDescription;
         this.fileName = fileName;
+        this.file = file;
     }
 
     public static FileBlockResponse from(FileBlock fileBlock) {
@@ -31,6 +33,7 @@ public class FileBlockResponse implements CommonResponse {
                 .fileTitle(fileBlock.getFileTitle())
                 .fileDescription(fileBlock.getFileDescription())
                 .fileName(fileBlock.getFileName())
+                .file(fileBlock.getFile())
                 .build();
     }
 }

705232a9e0e9a54d9196f3d44d854486da87ac73
Miyeon Lee <leeyun92@naver.com>
2023-10-14 23:59:38 +0900

    refactor : MemberGroupRepository에 멤버그룹리스트 응답을 위한 MemberGroupResponse타입의 리스트로 조회하는 메서드 구현 (#160)

--- src/main/java/com/javajober/space/dto/response/MemberGroupResponse.java
+++ src/main/java/com/javajober/space/dto/response/MemberGroupResponse.java
@@ -1,5 +1,7 @@
 package com.javajober.space.dto.response;
 
+import com.javajober.member.domain.AccountType;
+import com.javajober.memberGroup.domain.MemberHashtagType;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -16,11 +18,12 @@ public class MemberGroupResponse {
     }
 
     @Builder
-    public MemberGroupResponse(final Long memberId, final String memberName, final String memberHashtag, final String accountType, final String phoneNumber) {
+    public MemberGroupResponse(final Long memberId, final String memberName, final MemberHashtagType memberHashtag, final AccountType accountType, final String phoneNumber) {
         this.memberId = memberId;
         this.memberName = memberName;
-        this.memberHashtag = memberHashtag;
-        this.accountType = accountType;
+        this.memberHashtag = memberHashtag.name();
+        this.accountType = accountType.name();
         this.phoneNumber = phoneNumber;
     }
+
 }

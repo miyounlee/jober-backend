25161a1a7adf68b0e8c93f3e8740562fd12b84a6
Miyeon Lee <leeyun92@naver.com>
2023-10-15 00:01:49 +0900

    refactor : SpaceService 로직 리팩토링 (#160)

--- src/main/java/com/javajober/space/service/SpaceService.java
+++ src/main/java/com/javajober/space/service/SpaceService.java
@@ -1,80 +1,55 @@
 package com.javajober.space.service;
 
-import com.javajober.member.domain.Member;
-import com.javajober.member.repository.MemberRepository;
-import com.javajober.memberGroup.domain.MemberGroup;
-import com.javajober.memberGroup.domain.MemberHashtagType;
+import com.javajober.exception.ApiStatus;
+import com.javajober.exception.ApplicationException;
 import com.javajober.memberGroup.repository.MemberGroupRepository;
-import com.javajober.space.domain.AddSpace;
+import com.javajober.space.domain.SpaceType;
 import com.javajober.space.repository.AddSpaceRepository;
-import com.javajober.space.dto.response.DataResponse;
+import com.javajober.space.dto.response.SpaceResponse;
 import com.javajober.space.dto.response.MemberGroupResponse;
-import com.javajober.spaceWall.domain.SpaceWall;
+import com.javajober.spaceWall.domain.FlagType;
 import com.javajober.spaceWall.repository.SpaceWallRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class SpaceService {
 
-    private final MemberRepository memberRepository;
     private final MemberGroupRepository memberGroupRepository;
     private final AddSpaceRepository addSpaceRepository;
     private final SpaceWallRepository spaceWallRepository;
 
-    public SpaceService(final MemberRepository memberRepository,
-                        final MemberGroupRepository memberGroupRepository,
-                        final AddSpaceRepository addSpaceRepository,
-                        final SpaceWallRepository spaceWallRepository) {
-        this.memberRepository = memberRepository;
+    public SpaceService(final MemberGroupRepository memberGroupRepository, final AddSpaceRepository addSpaceRepository, final SpaceWallRepository spaceWallRepository) {
         this.memberGroupRepository = memberGroupRepository;
         this.addSpaceRepository = addSpaceRepository;
         this.spaceWallRepository = spaceWallRepository;
     }
 
     @Transactional
-    public DataResponse find (final Long memberId, final Long addSpaceId, final String spaceType) {
+    public SpaceResponse find(final Long memberId, final Long addSpaceId, final String spaceTypeString) {
 
-        AddSpace addSpace = addSpaceRepository.findAddSpace(addSpaceId);
-        Member member = memberRepository.findMember(memberId);
+        SpaceType spaceType = SpaceType.valueOf(spaceTypeString.toUpperCase());
+        List<Long> addSpaceIds = addSpaceRepository.findAddSpaceIds(spaceType, memberId); // 단체스페이스면 여러개 나옴
+        existsAddSpace(addSpaceId, addSpaceIds);
 
-        if (addSpace == null || member == null || !addSpace.getSpaceType().name().equalsIgnoreCase(spaceType)) {
-            throw new IllegalArgumentException("Invalid request parameters");
-        }
+        List<MemberGroupResponse> memberGroupResponses = memberGroupRepository.findMemberGroup(addSpaceId);
 
-        boolean hasWall = spaceWallRepository.existsByAddSpaceId(addSpaceId);
-        List<SpaceWall> spaceWalls = spaceWallRepository.findSpaceWalls(memberId, addSpaceId);
+        Optional<Long> spaceWallId = spaceWallRepository.findSpaceWallId(memberId, addSpaceId, FlagType.SAVED);
 
-        Long spaceWallId = null;
-        if (hasWall) {
-            List<SpaceWall> foundSpaceWalls = spaceWallRepository.findByAddSpaceId(addSpaceId);
-            if (!foundSpaceWalls.isEmpty()) {
-                spaceWallId = foundSpaceWalls.get(0).getId();
-            }
-        }
-
-        List<MemberGroup> memberGroups = memberGroupRepository.findAllByAddSpaceId(addSpaceId);
-        List<MemberGroupResponse> list = memberGroups.stream()
-                .sorted((mg1, mg2) -> {
-                    if (mg1.getMemberHashtagType() == MemberHashtagType.EMPTY) return -1;
-                    if (mg2.getMemberHashtagType() == MemberHashtagType.EMPTY) return 1;
-                    return mg1.getMemberHashtagType().compareTo(mg2.getMemberHashtagType());
-                })
-                .map(mg -> new MemberGroupResponse(
-                        mg.getMember().getId(),
-                        mg.getMember().getMemberName(),
-                        mg.getMemberHashtagType().toString(),
-                        mg.getAccountType().toString(),
-                        mg.getMember().getPhoneNumber()
-                )).collect(Collectors.toList());
-
-        return DataResponse.builder()
-                .hasWall(hasWall)
-                .spaceWallId(spaceWallId)
-                .list(list)
-                .build();
+        return spaceWallId.map(id -> new SpaceResponse(true, id, memberGroupResponses))
+                .orElseGet(() -> new SpaceResponse(false, null, memberGroupResponses));
     }
+
+    private static void existsAddSpace(Long addSpaceId, List<Long> addSpaceIds) {
+
+        boolean containsId = addSpaceIds.stream().anyMatch(id -> Objects.equals(id, addSpaceId));
+        if(!containsId) {
+            throw new ApplicationException(ApiStatus.NOT_FOUND, "존재하지 않는 스페이스 입니다.");
+        }
+    }
+
 }

42532089e54b7ec2ab824ee99ad1c1e4263bb913
Miyeon Lee <leeyun92@naver.com>
2023-10-22 12:25:19 +0900

    refactor : MoveBlockStrategy클래스에 createMoveBlockDTO메소드 추가 (#181)

--- src/main/java/com/javajober/spaceWall/strategy/MoveBlockStrategy.java
+++ src/main/java/com/javajober/spaceWall/strategy/MoveBlockStrategy.java
@@ -1,10 +1,14 @@
 package com.javajober.spaceWall.strategy;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.javajober.core.util.response.CommonResponse;
+
 import java.util.List;
 
 public interface MoveBlockStrategy {
 	List<Long> saveBlocks(final List<Object> subData);
 
+	List<CommonResponse> createMoveBlockDTO(List<JsonNode> blocksWithSamePosition);
 	String getStrategyName();
 }
 
